08f9f0b3c0adb4d978a4e5afddf4eda58ba76c9d
cderian <c.derian_11@ciencias.unam.mx>
2018-09-14 20:13:53 -0500

    Agregando condiciones a la parte de cifrado

--- Práctica 1/Hill.java
+++ Práctica 1/Hill.java
@@ -1,77 +1,125 @@
 import java.lang.Exception;
+/**
+ * Codificador y decodificador de textos con Cifrado de Hill.
+ *
+ * @author cderian
+ */
 
 public class Hill {
 
     /*
      * A B C D E F G H I J K  L  M  N  Ñ  O  P  Q  R  S  T  U  V  W  X  Y  Z 
      * 0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26
-    */
+     */
     static String alfabetoMayusculas = "ABCDEFGHIJKLMNÑOPQRSTUVWXYZ"; 
-    static int dim_clave = 3;
 
     /**
-     * 
+     * Obtiene el determinante de una matriz
+     * @param matriz la matriz a la cual se obtendrá su determinante.
+     * @return det el determinante de la matriz
+     */
+    private static int determinante(int[][] matriz){
+        int det = 0;
+
+        if(matriz.length == 2){
+            det = (matriz[0][0]*matriz[1][1]) - (matriz[0][1]*matriz[1][0]);
+        }
+
+        for(int i=0; i<matriz.length; i++){
+            int[][] nm = new int[matriz.length-1][matriz.length-1];
+
+            for(int j=0; j<matriz.length; j++){
+                if(j!=i){
+                    for(int k=1; k<matriz.length; k++){
+                        int indice=-1;
+                        if(j<i)
+                            indice=j;
+                        else if(j>i)
+                            indice=j-1;
+                        nm[indice][k-1]=matriz[j][k];
+                    }
+                }
+            }
+            if(i%2==0)
+                det+=matriz[i][0] * determinante(nm);
+            else
+                det-=matriz[i][0] * determinante(nm);
+        }
+
+        return det;
+    }
+
+    /**
+     * Obtiene el máximo común divisor de dos números.
+     * @param n el primer número.
+     * @param m el segundo número.
+     * @return res el mcd de n y m.
+     */
+    public static int mcd(int n, int m){
+        int a = Math.max(n,m);
+        int b = Math.min(n,m);
+        int res;
+
+        do{
+            res = b;
+            b = a%b;
+            a = res;
+        }while(b != 0);
+
+        return res;
+    }
+
+    /**
+     * Cifra un texto con Cifrado de Hill.
+     *
+     * Antes de cifrar tiene que verificar que la matriz clave sea cuadrada y que
+     * sea invertible en Zn (n = tamaño del alfabeto).
+     * Si el determinante de la matriz y el tamaño del alfabeto son números primos
+     * entonces, la matriz es invertible en Zn.
+     *
+     * @param cadena el texto que se cifrará
+     * @param llave la matriz clave que ayudará a cifrar
+     * @return rtnCadena el texto cifrado
      */
     private static String codificar(String cadena, int[][] llave) throws Exception{
         String rtnCadena = "";
 
+        int det = 0;
+
+        //La matriz debe ser cuadrada.
         if(llave.length != llave[0].length){
             throw new Exception("La matriz no es cuadrada");
+        }else{
+            det = determinante(llave);
         }
 
-        /*if(mcd(det,alfabetoMayusculas.length()) != 1){
-            String error = "El determinante de la matriz debe ser primo"
-            error+=        "con el tamaño del alfabeto"
-            error+=        "\n |Alfabeto| = " + alfabetoMayusculas.length();
-            error+=        "\n |M| = " + det +'\n';
-            throw Exception(error);
-        }*/
+        int mcd = mcd(det, alfabetoMayusculas.length());
+
+        //La matriz debe ser invertible en Z(27).
+        if(mcd != 1){
+            throw new Exception("El |M| y |Alfabeto| deben ser primos");
+        }
 
         String bloque = "";
         int i = llave.length;
         int[][] matriz_cif = new int[1][llave.length];
         int[][] matriz_nueva = new int[1][llave.length];
-            
-        //}
 
         while(cadena.length() > 0){
 
             if(i > cadena.length()){
-                System.out.println("entro if");
-                bloque = cadena.substring(i);
-
-                //Obtener valor numérico de las letras
-                for (int l=0; l<bloque.length(); l++) {
-                    int pos = alfabetoMayusculas.indexOf(bloque.charAt(l));
-                    matriz_cif[1][l] = pos;
-                }
-
-                //Obteniendo el valor numérico cifrado de las letras
-                for(int m1 = 0; m1 < llave.length; m1++){
-                    for(int m2 = 0; m2 < matriz_cif.length; m2++){
-                        matriz_nueva[1][m1]+= (m1*m2);
-                    }
-                }
-
-                //Obteniendo el valor numérico cifrado de las letras mod (long alfabeto)
-                for(int m = 0; m < llave.length; m++){
-                    matriz_nueva[1][m] = matriz_nueva[1][m]% alfabetoMayusculas.length();
-                }
-
-                //Traduciendo el valor numérico a letra
-                for(int m = 0; m < llave.length; m++){
-                    rtnCadena+=""+ alfabetoMayusculas.charAt(matriz_nueva[1][m]);
-                }
+                //Parte aún no funcional
+                break;
             }else{
                 bloque = cadena.substring(0, i);
 
-                //Obtener valor numérico de las letras
+                //Obteniendo valor numérico de las letras
                 for (int l=0; l<bloque.length(); l++) {
                     int pos = alfabetoMayusculas.indexOf(bloque.charAt(l));
                     matriz_cif[0][l] = pos;
                 }
 
-                //Obteniendo el valor numérico cifrado de las letras
+                //Obteniendo el valor numérico cifrado de las letras mod (long alfabeto)
                 for(int m1 = 0; m1 < llave.length; m1++){
                     int valor = 0;
 
@@ -81,11 +129,6 @@ public class Hill {
 
                     matriz_nueva[0][m1] = valor % alfabetoMayusculas.length();
                 }
-
-                //Obteniendo el valor numérico cifrado de las letras mod (long alfabeto)
-                /*for(int m = 0; m < llave[0].length; m++){
-                    matriz_nueva[0][m] = matriz_nueva[0][m]% alfabetoMayusculas.length();
-                }*/
 
                 //Traduciendo el valor numérico a letra
                 for(int m = 0; m < llave.length; m++){
@@ -98,31 +141,6 @@ public class Hill {
 
         return rtnCadena;
     }
-
-    /*private static int[][] obtenerMatriz(String bloque){
-        //Obtener valor numérico de las letras
-        for (int l=0; l<bloque.length(); l++) {
-            int pos = alfabetoMayusculas.indexOf(bloque.charAt(l));
-            matriz_cif[1][l] = pos;
-        }
-
-        //Obteniendo el valor numérico cifrado de las letras
-        for(int m1 = 0; m1 < llave.length; m1++){
-            for(int m2 = 0; m2 < matriz_cif.length; m2++){
-                matriz_nueva[1][m1]+= (m1*m2);
-            }
-        }
-
-        //Obteniendo el valor numérico cifrado de las letras mod (long alfabeto)
-        for(int m = 0; m < llave.length; m++){
-            matriz_nueva[1][m] = matriz_nueva[1][m]% alfabetoMayusculas.length();
-        }
-
-        //Traduciendo el valor numérico a letra
-        for(int m = 0; m < llave.length; m++){
-            rtnCadena+=""+ alfabetoMayusculas.charAt(matriz_nueva[1][m]);
-        }
-    }*/
 
     /**
      * 
@@ -139,42 +157,41 @@ public class Hill {
      */
     public static void main(String[] args) {
         try{
-        String mensaje = "CUADERNODECULTURACIENTIFICA";
-        String llave   = "FORTALEZA";
+            String mensaje = "CUADERNODECULTURACIENTIFICA";
+            String llave   = "FORTALEZA";
 
-        // 1. Texto original
-		System.out.println("Texto original:     " + mensaje);
-		System.out.println("Llave:              " + llave);
-        System.out.println();  
-              
+            // 1. Texto original
+            System.out.println("Texto original:     " + mensaje);
+            System.out.println("Llave:              " + llave);
+            System.out.println();  
+            
+            // 2. Codificar
+            //int[][] matriz = new int[3][2];  
 
-        // 2. Codificar
-        //int[][] matriz = new int[3][2];  
+            //Matriz 3x3 de Prueba 
+            int[][] matriz = new int[3][3];
+            matriz[0][0] = 1;
+            matriz[0][1] = 2;
+            matriz[0][2] = 3;
+            matriz[1][0] = 0;
+            matriz[1][1] = 4;
+            matriz[1][2] = 5;
+            matriz[2][0] = 1;
+            matriz[2][1] = 0;
+            matriz[2][2] = 6;
 
-        //Matriz 3x3 de Prueba 
-        int[][] matriz = new int[3][3];
-        matriz[0][0] = 1;
-        matriz[0][1] = 2;
-        matriz[0][2] = 3;
-        matriz[1][0] = 0;
-        matriz[1][1] = 4;
-        matriz[1][2] = 5;
-        matriz[2][0] = 1;
-        matriz[2][1] = 0;
-        matriz[2][2] = 6;
+            //NOTA: pueden agregar los métodos necesarios para obtener la matriz de la palabra clave
 
-        //NOTA: pueden agregar los métodos necesarios para obtener la matriz de la palabra clave
+            //int[][] matriz = obtenerClave(llave);
 
-        //int[][] matriz = obtenerClave(llave);
-
-        String mensajeCodificado = codificar(mensaje, matriz);
-        System.out.println("Texto codificado:   " + mensajeCodificado); 
-    }catch(Exception e){
-    }
+            String mensajeCodificado = codificar(mensaje, matriz);
+            System.out.println("Texto codificado:   " + mensajeCodificado); 
+        }catch(Exception e){
+            System.out.println(e.getMessage());
+        }
 
         // 3. Decodificar
         //String cadenaDecodificada = decodificar(mensaje, matriz, 2); 
         //System.out.println("Texto decodificado: " + cadenaDecodificada); 
     }
-
 }
